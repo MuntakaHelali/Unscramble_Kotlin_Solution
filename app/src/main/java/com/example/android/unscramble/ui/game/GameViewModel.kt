7d332deb3344c16b0573d9ead2cd633d6ce7295b
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-18 00:52:31 -0400

    Created getNextWord Function to get a random currentWord from the list of all words. Once the word is retrieved, it is shuffled. Once the word is shuffled, the currentWordCount is incremented by 1, and added to the wordList so that it will not be used again when getNextWord function is called.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,21 +1,55 @@
 package com.example.android.unscramble.ui.game
 
+import android.service.autofill.Dataset
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class GameViewModel: ViewModel() {
+
+    private var score = 0
+    private var _currentWordCount = 0
+    private lateinit var _currentScrambledWord: String
+    val currentScrambledWord: String
+        get() = _currentScrambledWord
+    private var wordList: MutableList<String> = mutableListOf()
+    private lateinit var currentWord: String
+
+    private fun getNextWord(){
+
+        currentWord = allWordsList.random()
+        val tempWord = currentWord.toCharArray()
+
+        while(String(tempWord) == currentWord) {
+           tempWord.shuffle()
+        }
+
+        if(wordList.contains(currentWord)){
+            getNextWord()
+        } else {
+            _currentScrambledWord = String(tempWord)
+            ++_currentWordCount
+            wordList.add(currentWord)
+        }
+
+    }
+
     init{
         Log.d("GameFragment","GameViewModel Created")
+        getNextWord()
     }
 
     override fun onCleared() {
         super.onCleared()
         Log.d("GameFragment", "GameViewModel Destroyed")
     }
-
-    private var score = 0
-    private var currentWordCount = 0
-    private var _currentScrambledWord = "test"
-    val currentScrambledWord: String
-        get() = _currentScrambledWord
+/*
+* Returns true if the current word count is less than MAX_NO_OF_WORDS.
+* Updates the next word.
+*/
+    fun nextWord(): Boolean{
+        return if(_currentWordCount < MAX_NO_OF_WORDS) {
+            getNextWord()
+            true
+        } else false
+    }
 }
