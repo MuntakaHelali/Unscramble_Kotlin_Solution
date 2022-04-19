ed4a4ad58fe0c993ccce09594450316e75e8006c
Muntaka Helali <helali.muntaka@gmail.com>
2022-04-19 02:50:31 -0400

    Added binding expression to the current scrambled word in the textView with the android:text property. Replicated the process with the wordCount and score values to their corresponding textViews. Finally removed all the observer code in the onCreateView() method since the binding expressions update the UI when the LiveData changes.

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -73,18 +73,18 @@ class GameFragment : Fragment() {
 //        binding.score.text = getString(R.string.score, 0)
 //        binding.wordCount.text = getString(
 //                R.string.word_count, 0, MAX_NO_OF_WORDS)
-//        Observe the currentScrambledWord LiveData
-        viewModel.currentScrambledWord.observe(viewLifecycleOwner) { newWord ->
-            binding.textViewUnscrambledWord.text = newWord
-        }
-//        Observe the score LiveData
-        viewModel.score.observe(viewLifecycleOwner){ newScore ->
-            binding.score.text = getString(R.string.score, newScore)
-        }
+////        Observe the currentScrambledWord LiveData
+//        viewModel.currentScrambledWord.observe(viewLifecycleOwner) { newWord ->
+//            binding.textViewUnscrambledWord.text = newWord
+//        }
+////        Observe the score LiveData
+//        viewModel.score.observe(viewLifecycleOwner){ newScore ->
+//            binding.score.text = getString(R.string.score, newScore)
+//        }
 //        Observe the wordCount LiveData
-        viewModel.currentWordCount.observe(viewLifecycleOwner){ newWordCount ->
-            binding.wordCount.text = getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
-        }
+//        viewModel.currentWordCount.observe(viewLifecycleOwner){ newWordCount ->
+//            binding.wordCount.text = getString(R.string.word_count, newWordCount, MAX_NO_OF_WORDS)
+//        }
 
     }
 
